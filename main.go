6e9ccf0f4bf72adccf295da25774bc4f1db481c2
Jeremy Corbett <jeremy.lael@gmail.com>
2012-02-13 23:22:41 -0500

    Printing out my new struct

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import(
   "math/cmplx"
 )
 
+type SingPert struct {
+  m,n,lambda complex128
+}
+
+func (p *SingPert) step(z complex128) complex128 {
+  zm := cmplx.Pow(z,p.m)
+  zn := cmplx.Pow(z,p.n)
+  return zm + p.lambda / zn
+}
+
 func main(){
   fmt.Println("hi")
-  fmt.Printf("%v",cmplx.Inf())
+  pert := SingPert{ 2,2,0.001i }
+  fmt.Printf("%v\n",pert)
 }
