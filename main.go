93bbc6a153981588fa3bfb7b7f662d4373104e09
Jeremy Corbett <jeremy.lael@gmail.com>
2012-02-13 23:31:31 -0500

    exporting step

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type SingPert struct {
   m,n,lambda complex128
 }
 
-func (p *SingPert) step(z complex128) complex128 {
+func (p *SingPert) Step(z complex128) complex128 {
   zm := cmplx.Pow(z,p.m)
   zn := cmplx.Pow(z,p.n)
   return zm + p.lambda / zn
@@ -19,5 +19,5 @@ func (p *SingPert) step(z complex128) complex128 {
 func main(){
   pert := SingPert{ 2,2,0.001i }
   fmt.Printf("%v\n",pert)
-  fmt.Printf("%v\n",pert.step(0.5+0.5i))
+  fmt.Printf("%v\n",pert.Step(0.5+0.5i))
 }
