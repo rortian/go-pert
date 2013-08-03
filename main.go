96eb8403368f37950b67052c76a829bf3926a971
Jeremy Corbett <jeremy.lael@gmail.com>
2013-08-03 19:37:11 -0400

    More dynamic stuff

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"image/png"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/rortian/go-pert/fractal"
@@ -29,26 +30,53 @@ func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.Handler
 	}
 }
 
+func parseFloat(s string, d float64) float64 {
+	p, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return d
+	}
+	return p
+}
+
+func parseInt(s string, d int64) int64 {
+	p, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return d
+	}
+	return p
+}
+
+func parseSingPert(r *http.Request) *fractal.SingPert {
+	var m = parseFloat(r.FormValue("m"), 2.0)
+	var n = parseFloat(r.FormValue("n"), 2.0)
+
+	var lambda_x = parseFloat(r.FormValue("lambda_x"), 1e-6)
+	var lambda_y = parseFloat(r.FormValue("lambda_y"), 0.0)
+
+	return &fractal.SingPert{complex(m, 0), complex(n, 0), complex(lambda_x, lambda_y)}
+}
+
+func parseGrid(r *http.Request, sp *fractal.SingPert) *fractal.Grid {
+	var width = parseInt(r.FormValue("width"), 500)
+	var height = parseInt(r.FormValue("height"), 500)
+
+	var x_min = parseFloat(r.FormValue("x_min"), -1.25)
+	var x_max = parseFloat(r.FormValue("x_max"), 1.25)
+
+	var y_min = parseFloat(r.FormValue("y_min"), -1.25)
+	var y_max = parseFloat(r.FormValue("y_max"), 1.25)
+
+	return &fractal.Grid{int(width), int(height), x_max, y_max, x_min, y_min, sp, &sync.WaitGroup{}}
+}
+
 func fractalHandler(w http.ResponseWriter, r *http.Request) error {
-	var m = 2.0
-	var n = 2.0
+	r.ParseForm()
 
-	var lambda_x = 1e-6
-	var lambda_y = 0.0
+	singpert := parseSingPert(r)
 
-	var width = 500
-	var height = 500
+	grid := parseGrid(r, singpert)
 
-	var x_min = -1.25
-	var x_max = 1.25
-
-	var y_min = -1.25
-	var y_max = 1.25
-
-	var pert = fractal.SingPert{complex(m, 0), complex(n, 0), complex(lambda_x, lambda_y)}
-	var grid2 = fractal.Grid{width, height, x_max, y_max, x_min, y_min, &pert, &sync.WaitGroup{}}
-
-	var img2 = image.NewNRGBA(image.Rect(0, 0, width, height))
+	img := image.NewNRGBA(image.Rect(0, 0, grid.X, grid.Y))
 
 	var red = color.NRGBA{0xFF, 0, 0, 0xFF}
 	var blue = color.NRGBA{0, 0xFF, 0, 0xFF}
@@ -56,11 +84,11 @@ func fractalHandler(w http.ResponseWriter, r *http.Request) error {
 
 	var simple = []color.Color{red, blue, green}
 
-	var first = &paint.SimplePaint{&paint.SimpColors{simple}, img2}
+	var first = &paint.SimplePaint{&paint.SimpColors{simple}, img}
 
-	first.PaintFrac(grid2.Solve())
+	first.PaintFrac(grid.Solve())
 
-	png.Encode(w, img2)
+	png.Encode(w, img)
 	return nil
 
 }
